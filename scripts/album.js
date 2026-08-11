0e481f599fe17336e651f046706b24f35446c821
LarryCode-tech <jo.bruckner@gmx.at>
2026-08-11 19:06:07 +0200

    Add files via upload

---
+++ scripts/album.js
@@ -0,0 +1,77 @@
+
+const galleryId = localStorage.getItem('gallery');
+
+console.log("GESPEICHERTE ID:", galleryId);
+
+console.log(
+    "ALLE ALBUM-IDS:",
+    gallery.map(album => album.id)
+);
+
+
+console.log(galleryId);
+
+let pictureHTML = '';
+
+gallery.forEach((album) => {
+
+  if (album.id === galleryId) {
+
+    album.pictures.forEach((picture) => {
+      pictureHTML +=
+      `<div class="grid-item grow">
+        <a href="#" data-featherlight="${picture}">
+          <img src="${picture}" alt="${album.description}" class="album-picture">
+        </a>
+      </div>`;
+    });
+
+    if(album.videos === true) {
+    album.videos.forEach((video) => {
+      pictureHTML +=
+      `<div class="grid-item grow">
+        <video class="album-movie" controls>
+          <source src="${video}" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+      </div>`;
+    });
+  }
+
+    const albumDescriptionHTML = `${album.albumDescription}`;
+
+    document.querySelector(".js-album-description").innerHTML = albumDescriptionHTML;
+
+    // document.getElementById("titellogo").innerHTML = albumDescriptionHTML;
+
+    document.querySelector(".js-photographer").innerHTML = album.photographerLink ?
+      `<p class="photographer">
+        Fotos von 
+        <a href= ${album.photographerLink} target="_blank">
+          ${album.photographer || 'leider keine Ahnung wem'} 
+        </a>
+      </p>` :
+      `<p class="photographer">
+        Fotos von 
+          ${album.photographer || 'leider keine Ahnung wem'}
+        </p>`;
+
+    document.querySelector(".js-banner").innerHTML =  `
+    	<source media="(max-width: 768px)" srcset="${album.titlePicture}">
+			<source media="(min-width: 769px)" srcset="${album.titlePictureQf}">
+			<img src="${album.titlePicture}" alt="Ein passendes Bild">`;
+
+
+  
+    }
+});
+
+document.querySelector(".js-grid-container").innerHTML = pictureHTML;
+
+
+
+function compensateScrollbar() {
+  const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+  document.documentElement.style.setProperty('--scrollbar-width', `${scrollbarWidth}px`);
+}
+
---
+++ scripts/gallerydaten.js
@@ -0,0 +1,66 @@
+const gallery = [
+      {
+    id: "74a00869-85ef-4e6b-bcb2-6c3bc1924993",
+    description: "U4",
+    albumDescription: "G5 Gig in legendären Räumlichkeiten",
+    titlePicture: "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_1.webp",
+    titlePictureQf: "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_1.webp",
+        pictures: [
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_1.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_2.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_3.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_4.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_5.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_6.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_7.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_8.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_9.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_10.webp",
+                "Bilder/gallerypics/U4_24/Majestätsbedarf_U4_11.webp",
+    ],
+    photographer: "Markus Pawlata",
+    // photographerLink: "https://www.instagram.com/user.1298074/"
+  },
+  {
+    id: "5d51168a-75bf-44b8-bb2e-a500ecfd8a9d",
+    description: "Szene Wien",
+    albumDescription: "Mit Butterbread und der KEB",
+    titlePicture: "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_2.webp",
+    titlePictureQf: "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_2.webp",
+        pictures: [
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_1.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_2.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_3.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_4.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_5.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_6.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_7.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_8.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_9.webp",
+                "Bilder/gallerypics/Szene_25/Majestätsbedarf_Szene_10.webp"
+
+    ],
+    photographer: "Max Peternell",
+    // photographerLink: "https://www.instagram.com/user.1298074/"
+  },
+    {
+   id: "2dec63cf-f25a-4307-af5d-c281e2c73af5",
+    description: "Aera",
+    albumDescription: "Mit OKFine, war toll!",
+    titlePicture: "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_13.webp",
+    titlePictureQf: "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_13.webp",
+        pictures: [
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_1.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_2.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_3.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_4.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_5.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_7.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_8.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_11.webp",
+                "Bilder/gallerypics/Aera_25/Majestätsbedarf_Aera_13.webp",
+    ],
+    photographer: "Max Peternell",
+    // photographerLink: "https://www.instagram.com/user.1298074/"
+  }
+];
